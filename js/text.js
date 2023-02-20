867ae0fd9842ff1485cafbc7f1c6040f07f86df9
Serina Wu <is.serina.wu@gmail.com>
2023-02-20 18:26:36 +0800

    显示过滤/排序后的结果

--- js/text.js
+++ js/text.js
@@ -16,11 +16,17 @@
 //         }
 //     }).mount('.demo2')
 
-    var app123  = new Vue ({
-        el: '#app',
-        data: {
-            message: '<a href="#">Hello Vue</a>',
-        }
-    });
 
-    Vue.createApp(app123).mount("#app");
+    const test = {
+        data() {
+            return {
+                numbers: [ 1, 2, 3, 4, 5]
+            }
+        },
+        computed: {
+            evenNumbers() {
+                return this.numbers.filter(number => number % 2 === 0)
+            }
+        }
+    }
+    Vue.createApp(test).mount('#app')
