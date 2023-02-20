c642b110dad6e5ca803112d7694e6932ce091506
Serina Wu <is.serina.wu@gmail.com>
2023-02-20 15:49:52 +0800

    兩種寫法相通

--- js/text.js
+++ js/text.js
@@ -1,9 +1,26 @@
-import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
+// import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
         
-    createApp({
-        data() {
-            return {
-                message: 'Hello Vue!'
-            }
+//     createApp({
+//         data() {
+//             return {
+//                 message: 'Hello Vue!'
+//             }
+//         }
+//     }).mount('.demo')
+
+//     createApp({
+//         data() {
+//             return {
+//                 message: 'Hello 123123'
+//             }
+//         }
+//     }).mount('.demo2')
+
+    var app123  = new Vue ({
+        el: '#app',
+        data: {
+            message: '<a href="#">Hello Vue</a>',
         }
-    }).mount('#app')
+    });
+
+    Vue.createApp(app123).mount("#app");
