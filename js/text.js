f22f65d1c32c87412309e9a2dac69cfc7c27d1d3
wawatai <is.serina.wu@gmail.com>
2023-02-22 15:00:42 +0800

    監聽: 單位換算器

--- js/text.js
+++ js/text.js
@@ -19,12 +19,30 @@
 const app = {
   data() {
     return {
-      counter: 1
+      kilometers: 0,
+      meters: 0
+    }
+  },
+  watch : {
+    kilometers: function(newValue, oldValue) {
+      //判断是否是当前输入框
+      if (this.currentlyActiveField === 'kilometers') {
+        this.kilometers = newValue;
+        this.meters = newValue * 1000;
+      }
+    },
+    meters: function(newValue, oldValue) {
+      //判断是否是当前输入框
+      if (this.currentlyActiveField === 'meters') {
+        this.kilometers = newValue/ 1000;
+        this.meters = newValue;
+      }
     }
   }
 }
 
 const vm = Vue.createApp(app).mount('#app')
-vm.$watch('counter', function(nval, oval) {
-  alert('計數器值的變化:' + oval + '變為' + nval + '!');
-});
+vm.$watch('kilometers', function(newValue, oldValue) {
+  // 这个回调将在 vm.kilometers 改变后调用
+  document.getElementById ("info").innerHTML = "修改前值為: " + oldValue + "，修改後值為:" + newValue; 
+})
