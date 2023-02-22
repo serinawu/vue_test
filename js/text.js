16ce2e00545f286699fd8801c3b003aa35c2d94b
wawatai <is.serina.wu@gmail.com>
2023-02-22 16:31:58 +0800

    引入樣式
    :class & v-bind:class 都可以
    原本的class也可以保留

--- js/text.js
+++ js/text.js
@@ -1,4 +1,7 @@
 // import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
+
+// const { VueElement } = require("vue")
+
         
 //     createApp({
 //         data() {
@@ -19,30 +22,10 @@
 const app = {
   data() {
     return {
-      kilometers: 0,
-      meters: 0
-    }
-  },
-  watch : {
-    kilometers: function(newValue, oldValue) {
-      //判断是否是当前输入框
-      if (this.currentlyActiveField === 'kilometers') {
-        this.kilometers = newValue;
-        this.meters = newValue * 1000;
-      }
-    },
-    meters: function(newValue, oldValue) {
-      //判断是否是当前输入框
-      if (this.currentlyActiveField === 'meters') {
-        this.kilometers = newValue/ 1000;
-        this.meters = newValue;
-      }
+      isActive: false,
+      hasError: true
     }
   }
 }
 
-const vm = Vue.createApp(app).mount('#app')
-vm.$watch('kilometers', function(newValue, oldValue) {
-  // 这个回调将在 vm.kilometers 改变后调用
-  document.getElementById ("info").innerHTML = "修改前值為: " + oldValue + "，修改後值為:" + newValue; 
-})
+Vue.createApp(app).mount('#app')
