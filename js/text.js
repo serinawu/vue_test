186da1a1f909409d9375aed4f6d7875b60fb450a
wawatai <62425140+serinawu@users.noreply.github.com>
2023-03-01 17:20:39 +0800

    Ajax 測試!!

--- js/text.js
+++ js/text.js
@@ -17,31 +17,20 @@
 //             }
 //         }
 //     }).mount('.demo2')
+const app = {
+  data() {
+    return {
+      info: 'Ajax 測試!!'
+    }
+  },
+  mounted () {
+    axios
+    .get('https://www.runoob.com/try/ajax/json_demo.json')
+    .then(response => (this.info = response))
+    .catch(function (error) {
+      console.log(error);
+    })
+  }
+}
 
-// 1. 定义路由组件.
-// 也可以从其他文件导入
-const Home = { template: '<div>Home</div>' }
-const About = { template: '<div>About</div>' }
- 
-// 2. 定义一些路由
-// 每个路由都需要映射到一个组件。
-const routes = [
-  { path: '/', component: Home },
-  { path: '/about', component: About },
-]
- 
-// 3. 创建路由实例并传递 `routes` 配置
-// 你可以在这里输入更多的配置，但我们在这里暂时保持简单
-const router = VueRouter.createRouter({
-  // 4. 内部提供了 history 模式的实现。为了简单起见，我们在这里使用 hash 模式。
-  history: VueRouter.createWebHashHistory(),
-  routes, // `routes: routes` 的缩写
-})
- 
-// 5. 创建并挂载根实例
-const app = Vue.createApp({})
-//确保 _use_ 路由实例使
-//整个应用支持路由。
-app.use(router)
- 
-app.mount('#app')
+Vue.createApp(app).mount('#app')
