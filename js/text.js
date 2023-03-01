a6486df1524550cb8c9bd51f6868b531245522bc
wawatai <62425140+serinawu@users.noreply.github.com>
2023-03-01 15:16:17 +0800

    參數使用演練(成功，但不曉得部分原理)

--- js/text.js
+++ js/text.js
@@ -1,9 +1,6 @@
 // import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
 
-const { VueElement } = require("vue")
-
 // const { VueElement } = require("vue")
-
         
 //     createApp({
 //         data() {
@@ -21,33 +18,11 @@ const { VueElement } = require("vue")
 //         }
 //     }).mount('.demo2')
 
-//方法一: 全域
-// const app = Vue.createApp({})
-// // 註冊一個全局自訂義指令 'v-focus'
-// app.directive('focus', {
-//   //當被綁定的元素掛載到DOM時....
-//   mounted(el) {
-//     //聚焦元素
-//     el.focus()
-//   }
-// })
-
-// app.mount('#app')
-
-//方法二: 局部指令
-const app = {
-  data() {
-     return {
-     }
-  },
-  directives: {
-     focus: {
-        // 指令的定义
-        mounted(el) {
-           el.focus()
-        }
-     }
-  }
-}
-
-Vue.createApp(app).mount('#app')
+const app = Vue.createApp({})
+app.directive('runoob', (el, binding, vnode) => {
+  console.log(binding.value.name)
+  console.log(binding.value.url) 
+  var s = JSON.stringify
+  el.innerHTML = s(binding.value)
+})
+app.mount('#app')
