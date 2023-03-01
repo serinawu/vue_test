cdf41498566d8f8f6a212f2cd96a99945aeb8427
wawatai <62425140+serinawu@users.noreply.github.com>
2023-03-01 15:01:11 +0800

    自訂義指令-頁面重整自動鎖定

--- js/text.js
+++ js/text.js
@@ -1,5 +1,7 @@
 // import { createApp } from 'https://unpkg.com/vue@3/dist/vue.esm-browser.js'
 
+const { VueElement } = require("vue")
+
 // const { VueElement } = require("vue")
 
         
@@ -19,25 +21,33 @@
 //         }
 //     }).mount('.demo2')
 
+//方法一: 全域
+// const app = Vue.createApp({})
+// // 註冊一個全局自訂義指令 'v-focus'
+// app.directive('focus', {
+//   //當被綁定的元素掛載到DOM時....
+//   mounted(el) {
+//     //聚焦元素
+//     el.focus()
+//   }
+// })
+
+// app.mount('#app')
+
+//方法二: 局部指令
 const app = {
   data() {
-    return {
-      checked: false,
-      checkedNames: [],
-      picked : 'Runoob',
-      selected: '',
-      chossed: '',
-      select: 'https://facebook.com',
-      options: [
-        {text: 'facebook', value: 'https://facebook.com'},
-        {text: 'instagram', value: 'https://instagram.com'},
-        {text: 'twitter', value: 'https://twitter.com'}
-      ]
-    }
+     return {
+     }
+  },
+  directives: {
+     focus: {
+        // 指令的定义
+        mounted(el) {
+           el.focus()
+        }
+     }
   }
 }
-
-
-
 
 Vue.createApp(app).mount('#app')
